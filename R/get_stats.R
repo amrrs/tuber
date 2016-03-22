05982a5fbb9446b1a9cf7f57c3c36c1bbed8a20d
amrrs <amrrs@outlook.com>
2016-03-22 16:49:45 +0530

    trying to add new metrics

--- R/get_stats.R
+++ R/get_stats.R
@@ -26,6 +26,7 @@ get_stats <- function (video_id=NULL) {
 	cat('No. of Dislikes', res$dislikeCount, "\n")
 	cat('No. of Favorites', res$favoriteCount, "\n")
 	cat('No. of Comments', res$commentCount, "\n")
+	cat('averageViewDuration', res$averageViewDurationCount, "\n")
 
 	return(invisible(res))
 }
